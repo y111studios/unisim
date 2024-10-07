43fbd6becb16b7eea24517a39c0394755c96f89c
Roboss11 <sam.knight0411@gmail.com>
2024-10-07 20:39:52 +0100

    Initial maven project setup

---
+++ src/test/java/y111studios/AppTest.java
@@ -0,0 +1,12 @@
+package y111studios;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
