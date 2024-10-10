c2e7d99d59f1f79bbbd2fe20e147b936ea1d8178
Tempestar11 <153299874+Tempestar11@users.noreply.github.com>
2024-10-10 17:51:39 +0100

    Created asset folder and implemented LibGDX asset loading

---
+++ src/main/java/y111studios/main.java
@@ -0,0 +1,22 @@
+package y111studios;
+
+// Loading assests at game start
+public class Main implements ApplicationListener {
+    // Templates
+    // Texture backgroundTexture;
+    // Texture bucketTexture;
+    // Texture dropTexture;
+    // Sound dropSound;
+    // Music music;
+    // Templates
+}
+
+// Create assets objects here
+@Override
+public void create() {
+    // Templates
+    // backgroundTexture = new Texture("background.png");
+    // bucketTexture = new Texture("bucket.png");
+    // dropTexture = new Texture("drop.png");
+    // Templates
+}
