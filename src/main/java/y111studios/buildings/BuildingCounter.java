07c910555226f3c3be2333202e5b65e68c4b3085
Ashish Kumar <hnr510@york.ac.uk>
2024-10-20 00:31:27 +0100

    Added a simple BuildingCounter

---
+++ src/main/java/y111studios/buildings/BuildingCounter.java
@@ -0,0 +1,70 @@
+package y111studios.buildings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A building counter to keep track of the number of buildings placed on the university campus.
+ */
+public class BuildingCounter {
+
+    public enum BuildingType {
+        ACCOMMODATION,
+        CATERING,
+        RECREATION,
+        TEACHING;
+    }
+
+    private final Map<BuildingType, Integer> buildingMap; // A dictionary to store the count of each building type.
+    private int count; // A field to track the total number of buildings
+
+    /**
+     * Initializes the building counter with the numbers of all building types set to 0.
+     */
+    public BuildingCounter() {
+        buildingMap = new HashMap<>();
+        for (BuildingType type : BuildingType.values()) {
+            buildingMap.put(type, 0);
+        }
+        count = 0; // Initialise the total number of buildings to be 0.
+    }
+
+    /**
+     * Increments the count for the specified building type.
+     * 
+     * @param type The type of building to increment.
+     */
+    public void placeBuilding(BuildingType type) {
+        buildingMap.put(type, buildingMap.get(type) + 1);
+        count++; // Increment the total number of buildings when one is placed.
+    }
+
+    /**
+     * Returns the count for the specified building type.
+     * 
+     * @param type The type of building to retrieve the count for.
+     * @return The count for the specified building type.
+     */
+    public int getBuildingCount(BuildingType type) {
+        return buildingMap.get(type);
+    }
+
+    /**
+     * Returns the total number of buildings placed.
+     * 
+     * @return The total number of buildings placed.
+     */
+    public int getTotalCount() {
+        return count;
+    }
+
+    /**
+     * Resets the building counter to its initial state, setting all building counts to 0 and total buildings placed to 0.
+     */
+    public void reset() {
+        for (BuildingType type : BuildingType.values()) {
+            buildingMap.put(type, 0);
+        }
+    }
+    
+}
