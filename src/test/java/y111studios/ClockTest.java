e0b529fc132dee1ca6ec2182ed2564a5b3cd71ba
Roboss11 <sam.knight0411@gmail.com>
2024-10-21 12:23:22 +0100

    Added Clock tests

---
+++ src/test/java/y111studios/ClockTest.java
@@ -0,0 +1,38 @@
+package y111studios;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+public class ClockTest {
+
+    @Test
+    void clockCreation() {
+        assertDoesNotThrow(() -> new Clock());
+        Clock clock = new Clock();
+        assertNotNull(clock);
+    }
+
+    @Test
+    void isTimeUp() {
+        Instant start = Instant.now();
+        Clock clock = new Clock(start);
+
+        // Test clock isn't immediately finished
+        assertFalse(clock.isTimeUp());
+
+        // Test clock isn't finished part way through
+        Instant later = start.plusSeconds(120);
+        clock = new Clock(later);
+        assertFalse(clock.isTimeUp());
+
+        // Test clock is finished after 5 minutes
+        Instant end = start.minusSeconds(300);
+        clock = new Clock(end);
+        assertTrue(clock.isTimeUp());
+    }
+}
