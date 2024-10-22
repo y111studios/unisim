6e2f1e5772745db0990d2fa18c493eed7d603be2
Roboss11 <sam.knight0411@gmail.com>
2024-10-22 21:12:19 +0100

    Added method to convert from Building instance to BuildingType

--- src/main/java/y111studios/buildings/BuildingType.java
+++ src/main/java/y111studios/buildings/BuildingType.java
@@ -1,8 +1,18 @@
 package y111studios.buildings;
 
 public enum BuildingType {
-    ACCOMMODATION,
-    CATERING,
-    RECREATION,
-    TEACHING;
+    ACCOMMODATION, CATERING, RECREATION, TEACHING;
+
+    public static BuildingType fromBuilding(Building building) {
+        if (building instanceof AccomodationBuilding) {
+            return ACCOMMODATION;
+        } else if (building instanceof CateringBuilding) {
+            return CATERING;
+        } else if (building instanceof RecreationBuilding) {
+            return RECREATION;
+        } else if (building instanceof TeachingBuilding) {
+            return TEACHING;
+        }
+        throw new IllegalArgumentException("Building type not recognised.");
+    }
 }
---
+++ src/test/java/y111studios/buildings/BuildingTypeTest.java
@@ -0,0 +1,33 @@
+package y111studios.buildings;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import y111studios.buildings.premade_variants.AccomodationVariant;
+import y111studios.buildings.premade_variants.CateringVariant;
+import y111studios.buildings.premade_variants.RecreationVariant;
+import y111studios.buildings.premade_variants.TeachingVariant;
+import y111studios.position.GridPosition;
+
+public class BuildingTypeTest {
+
+    @Test
+    void fromBuilding() {
+        final GridPosition ZERO = new GridPosition(0, 0);
+        Building building;
+
+        building =
+                BuildingFactory.createAccomodationBuilding(AccomodationVariant.SMALL_HOUSE, ZERO);
+        assertEquals(BuildingType.ACCOMMODATION, BuildingType.fromBuilding(building));
+
+        building = BuildingFactory.createCateringBuilding(CateringVariant.FAST_FOOD, ZERO);
+        assertEquals(BuildingType.CATERING, BuildingType.fromBuilding(building));
+
+        building = BuildingFactory.createRecreationBuilding(RecreationVariant.PARK, ZERO);
+        assertEquals(BuildingType.RECREATION, BuildingType.fromBuilding(building));
+
+        building = BuildingFactory.createTeachingBuilding(TeachingVariant.SMALL_CLASSROOM, ZERO);
+        assertEquals(BuildingType.TEACHING, BuildingType.fromBuilding(building));
+
+    }
+
+}
