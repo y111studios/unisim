8bc4ebfddc5576083502896a5ed6e5e70b819529
Roboss11 <sam.knight0411@gmail.com>
2024-10-24 13:11:24 +0100

    Added contains function to GridArea for a GridPosition

--- src/main/java/y111studios/position/GridArea.java
+++ src/main/java/y111studios/position/GridArea.java
@@ -27,4 +27,34 @@ public class GridArea {
         this(new GridPosition(x, y), width, height);
     }
 
+    /**
+     * Checks if the specified GridPosition is within this GridArea.
+     * 
+     * <p>
+     * This method returns true if the x-coordinate of the other position is within the range
+     * [origin.x, origin.x + width) and the y-coordinate is within the range [origin.y, origin.y +
+     * height). If other is {@Code null}, this method returns false.
+     * </p>
+     * 
+     * @param other the GridPosition to check
+     * @return true if the specified position is within this area, false otherwise
+     */
+    public boolean contains(GridPosition other) {
+        if (other == null) {
+            return false;
+        }
+        GridPosition end = getEndPosition();
+        return other.getX() >= origin.getX() && other.getX() < end.getX()
+                && other.getY() >= origin.getY() && other.getY() < end.getY();
+    }
+
+    /**
+     * Gets the GridPosition for the bottom-right corner of this area.
+     * 
+     * @return GridPosition representing the opposite corner to the origin
+     */
+    private GridPosition getEndPosition() {
+        return new GridPosition(origin.getX() + width, origin.getY() + height);
+    }
+
 }
