e6178e78d3681fd2afccaa26289696550f159b2e
Roboss11 <sam.knight0411@gmail.com>
2024-10-29 23:57:12 +0000

    Implemented BuildingController interface for GameState

--- src/main/java/y111studios/GameState.java
+++ src/main/java/y111studios/GameState.java
@@ -1,10 +1,14 @@
 package y111studios;
 
 import java.time.Duration;
+import y111studios.buildings.Building;
+import y111studios.buildings.BuildingController;
 import y111studios.buildings.BuildingManager;
 import y111studios.clock.Clock;
 import y111studios.clock.GameTimer;
 import y111studios.map.CollisionDetection;
+import y111studios.position.GridArea;
+import y111studios.position.GridPosition;
 
 /**
  * A class representing the sum state of the game. This class contains the clock, building manager
@@ -15,14 +19,14 @@ import y111studios.map.CollisionDetection;
  * @see BuildingManager
  * @see CollisionDetection
  */
-public class GameState implements GameTimer {
+public class GameState implements GameTimer, BuildingController {
 
     private GameTimer timer;
     BuildingManager buildingManager;
     CollisionDetection collisionDetection;
 
     /**
-     * Constructor for the GameState class. Initializes
+     * Constructor for the GameState class.
      * 
      * @param width width of the game map
      * @param height height of the game map
@@ -31,6 +35,73 @@ public class GameState implements GameTimer {
         timer = new Clock();
         buildingManager = new BuildingManager();
         collisionDetection = new CollisionDetection(width, height);
+    }
+
+    // BuildingController methods
+
+    @Override
+    public Building getBuilding(GridPosition position) {
+        if (position == null) {
+            return null;
+        }
+        if (collisionDetection.canPlaceBuilding(position.getX(), position.getY(), 1, 1)) {
+            return null;
+        }
+        return this.buildingManager.getBuilding(position);
+    }
+
+    @Override
+    public boolean push(Building building) {
+        if (building == null) {
+            return true;
+        }
+        // Cannot push into a full building manager
+        if (buildingManager.isFull()) {
+            return false;
+        }
+        GridArea buildingArea = building.getArea();
+        // Cannot push if the building area is not empty
+        if (collisionDetection.canPlaceBuilding(buildingArea.getX(), buildingArea.getY(),
+                buildingArea.getWidth(), buildingArea.getHeight()) == false) {
+            return false;
+        }
+        // Push the building into the building mangager
+        this.buildingManager.push(building);
+        return true;
+    }
+
+    @Override
+    public boolean removePosition(GridPosition position) {
+        if (position == null) {
+            return false;
+        }
+        // Check if the origin point is used
+        if (collisionDetection.canPlaceBuilding(position.getX(), position.getY(), 1, 1)) {
+            return false;
+        }
+        // Get building being removed
+        final Building building = this.buildingManager.getBuilding(position);
+        if (building == null) {
+            // This should never happen provided push is correctly implemented
+            throw new IllegalStateException("Building not found at position: " + position);
+        }
+        // Extract the area the building occupies
+        GridArea buildingArea = building.getArea();
+        // Remove the building from the collision detection
+        this.collisionDetection.removeBuilding(buildingArea.getX(), buildingArea.getY(),
+                buildingArea.getWidth(), buildingArea.getHeight());
+        this.buildingManager.removePosition(position);
+        return true;
+    }
+
+    @Override
+    public int getCount() {
+        return this.buildingManager.getCount();
+    }
+
+    @Override
+    public boolean isFull() {
+        return this.buildingManager.isFull();
     }
 
     // GameTimer methods
@@ -59,5 +130,6 @@ public class GameState implements GameTimer {
     public Duration timeRemaining() {
         return timer.timeRemaining();
     }
+
 
 }
