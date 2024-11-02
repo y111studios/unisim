414e2568b4a57eb150f441897ece9812ac22a8d1
Roboss11 <sam.knight0411@gmail.com>
2024-11-02 00:46:11 +0000

    Extended CollisionDetection tests to cover non-square maps

--- src/test/java/y111studios/map/CollisionDetectionTest.java
+++ src/test/java/y111studios/map/CollisionDetectionTest.java
@@ -1,47 +1,51 @@
 package y111studios.map;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class CollisionDetectionTest {
 
-    @Test
-    void canPlaceBuilding() {
-        CollisionDetection cd = new CollisionDetection(10, 10);
-        // Typical case that should work
-        assertTrue(cd.canPlaceBuilding(1, 1, 3, 3));
-        // Case of x being out of bounds
-        assertDoesNotThrow(() -> cd.canPlaceBuilding(10, 8, 1, 1));
-        assertFalse(cd.canPlaceBuilding(10, 8, 1, 1));
-        // Case of y being out of bounds
-        assertDoesNotThrow(() -> cd.canPlaceBuilding(8, 10, 1, 1));
-        assertFalse(cd.canPlaceBuilding(10, 8, 1, 1));
-        // Case of x + width being out of bounds
-        assertDoesNotThrow(() -> cd.canPlaceBuilding(9, 8, 10, 1));
-        assertFalse(cd.canPlaceBuilding(10, 8, 1, 1));
-        // Case of y + height being out of bounds
-        assertDoesNotThrow(() -> cd.canPlaceBuilding(8, 9, 1, 10));
-        assertFalse(cd.canPlaceBuilding(10, 8, 1, 1));
-        // Case of negative x
-        assertDoesNotThrow(() -> cd.canPlaceBuilding(-1, 8, 1, 1));
-        assertFalse(cd.canPlaceBuilding(-1, 8, 1, 1));
-        // Case of negative y
-        assertDoesNotThrow(() -> cd.canPlaceBuilding(8, -1, 1, 1));
-        assertFalse(cd.canPlaceBuilding(8, -1, 1, 1));
-        // Case of negative width
-        assertDoesNotThrow(() -> cd.canPlaceBuilding(8, 8, -1, 1));
-        assertFalse(cd.canPlaceBuilding(8, 8, -1, 1));
-        // Case of negative height
-        assertDoesNotThrow(() -> cd.canPlaceBuilding(8, 8, 1, -1));
-        assertFalse(cd.canPlaceBuilding(8, 8, 1, -1));
-        // Case of 0 width
-        assertDoesNotThrow(() -> cd.canPlaceBuilding(8, 8, 0, 1));
-        assertFalse(cd.canPlaceBuilding(8, 8, 0, 1));
-        // Case of 0 height
-        assertDoesNotThrow(() -> cd.canPlaceBuilding(8, 8, 1, 0));
-        assertFalse(cd.canPlaceBuilding(8, 8, 1, 0));
+    @ParameterizedTest
+    @MethodSource("provideCanPlaceBuildingCases")
+    void canPlaceBuilding(CollisionDetection cd, int x, int y, int width, int height,
+            boolean expected) {
+        assertEquals(expected, cd.canPlaceBuilding(x, y, width, height));
+    }
+
+    private static Stream<Arguments> provideCanPlaceBuildingCases() {
+        CollisionDetection square = new CollisionDetection(10, 10);
+        Stream<Arguments> squareCases = Stream.of(
+            Arguments.of(square, 1, 1, 3, 3, true), // Typical case
+            Arguments.of(square, 0, 0, 4, 4, true), // Case: start corner of map
+            Arguments.of(square, 9, 9, 1, 1, true), // Case: end corner of map
+            Arguments.of(square, 10, 8, 1, 1, false), // Case: x out of bounds
+            Arguments.of(square, 8, 10, 1, 1, false), // Case: y out of bounds
+            Arguments.of(square, 9, 8, 10, 1, false), // Case: x + width out of bounds
+            Arguments.of(square, 8, 9, 1, 10, false), // Case: y + height out of bounds
+            Arguments.of(square, -1, 8, 1, 1, false), // Case: negative x
+            Arguments.of(square, 8, -1, 1, 1, false), // Case: negative y
+            Arguments.of(square, 8, 8, -1, 1, false), // Case: negative width
+            Arguments.of(square, 8, 8, 1, -1, false), // Case: negative height
+            Arguments.of(square, 8, 8, 0, 1, false), // Case: 0 width
+            Arguments.of(square, 8, 8, 1, 0, false) // Case: 0 height
+        );
+        CollisionDetection yRect = new CollisionDetection(10, 20);
+        Stream<Arguments> yRectCases = Stream.of(
+            Arguments.of(yRect, 1, 1, 3, 3, true), // Typical case
+            Arguments.of(yRect, 4, 15, 4, 3, true), // Case: correct check of y vs. height
+            Arguments.of(yRect, 15, 6, 2, 3, false) // Case: correct check of x vs. width
+        );
+        CollisionDetection xRect = new CollisionDetection(20, 10);
+        Stream<Arguments> xRectCases = Stream.of(
+            Arguments.of(xRect, 1, 1, 3, 3, true), // Typical case
+            Arguments.of(xRect, 15, 4, 3, 4, true), // Case: correct check of y vs. height
+            Arguments.of(xRect, 6, 15, 3, 2, false) // Case: correct check of x vs. width
+        );
+        Stream<Arguments> rectCases = Stream.concat(xRectCases, yRectCases);
+        return Stream.concat(squareCases, rectCases);
     }
 
 }
