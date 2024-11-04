7cdddac1eb96136a05b4b7d2be1a84a2befb87cd
Roboss11 <sam.knight0411@gmail.com>
2024-11-04 23:15:29 +0000

    Added docstring to UnreachableException

--- src/main/java/y111studios/utils/UnreachableException.java
+++ src/main/java/y111studios/utils/UnreachableException.java
@@ -1,5 +1,14 @@
 package y111studios.utils;
 
+/**
+ * An exception that is thrown when code that is presumed to be unreachable is executed.
+ * 
+ * <p>
+ * This exception is used to indicate that a certain piece of code should never be executed. If it
+ * is then one of the assumed invariants of the program has been violated and either the case needs
+ * to be handled or the code needs to be fixed.
+ * </p>
+ */
 public class UnreachableException extends RuntimeException {
 
     private static final String DEFAULT_MESSAGE = "Presumed unreachable code executed";
