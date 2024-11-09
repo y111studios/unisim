e99737a9d91d5d2078df108c9f8684e13cbe3f6a
Roboss11 <sam.knight0411@gmail.com>
2024-11-09 17:30:34 +0000

    Added documentation to GridPosition

--- src/main/java/y111studios/position/GridPosition.java
+++ src/main/java/y111studios/position/GridPosition.java
@@ -2,11 +2,22 @@ package y111studios.position;
 
 import lombok.Getter;
 
+/**
+ * Represents a position on a grid.
+ */
 public class GridPosition {
 
     private @Getter int x;
     private @Getter int y;
 
+    /**
+     * Creates a new grid position.
+     *
+     * @param x the x coordinate
+     * @param y the y coordinate
+     * @throws IllegalArgumentException if x is negative
+     * @throws IllegalArgumentException if y is negative
+     */
     public GridPosition(int x, int y) {
         if (x < 0) {
             throw new IllegalArgumentException("X must not be negative");
