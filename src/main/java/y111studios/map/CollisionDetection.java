3e5f3d7f6b8e781aa4ee83c95b3dfae1d1bfadc7
Roboss11 <sam.knight0411@gmail.com>
2024-11-09 17:17:28 +0000

    Updated CollisionDetection documentation

--- src/main/java/y111studios/map/CollisionDetection.java
+++ src/main/java/y111studios/map/CollisionDetection.java
@@ -1,13 +1,34 @@
 package y111studios.map;
 
-// 2D array - True = tile occupied, False = tile empty
 import y111studios.position.GridArea;
 import y111studios.position.GridPosition;
 
+/**
+ * A class for detecting collisions between buildings on the map.
+ * 
+ * <p>
+ * The collision detection class is used to determine if a building can be placed on the map given a
+ * position and area. The class also provides methods for updating the internal data of this class
+ * to reflect the new building placements.
+ * </p>
+ */
 public class CollisionDetection {
+    /**
+     * Boolean array representing the grid of the map. This is a false initialized grid where true
+     * values represent occupied cells, and false values represent empty cells.
+     */
     private boolean[][] buildingGrid;
+    /**
+     * The total area of the map. This is used to determine the bounds of the map.
+     */
     private GridArea mapArea;
 
+    /**
+     * Constructs a new collision detection object with the specified width and height.
+     * 
+     * @param width the width of the map
+     * @param height the height of the map
+     */
     public CollisionDetection(int width, int height) {
         buildingGrid = new boolean[width][height];
         mapArea = new GridArea(0, 0, width, height);
@@ -23,6 +44,12 @@ public class CollisionDetection {
         return mapArea.contains(area);
     }
 
+    /**
+     * Checks if a given position is within the bounds of the map.
+     * 
+     * @param position the position to check
+     * @return true if the position is within the bounds of the map, false otherwise
+     */
     private boolean withinBounds(GridPosition position) {
         return mapArea.contains(position);
     }
@@ -46,6 +73,13 @@ public class CollisionDetection {
         return true;
     }
 
+    /**
+     * Checks if the given position is a valid place to build. This method checks only a single
+     * cell.
+     * 
+     * @param position the position to check
+     * @return true if the position is a valid place to build, false otherwise
+     */
     public boolean canPlaceBuilding(GridPosition position) {
         if (!withinBounds(position)) {
             return false;
@@ -54,8 +88,8 @@ public class CollisionDetection {
     }
 
     /**
-     * Attempts to place a building on the grid in the given area. If the build placed, it marks the
-     * grid cells as occupied.
+     * Attempts to place a building on the grid in the given area. If the building can be placed, it
+     * marks the grid cells as occupied.
      *
      * @param area the area to place the building
      */
@@ -75,6 +109,12 @@ public class CollisionDetection {
         fillArea(area, false);
     }
 
+    /**
+     * Fills the given area of the grid with the specified value.
+     * 
+     * @param area the area to fill
+     * @param value the value to fill the area with
+     */
     private void fillArea(GridArea area, boolean value) {
         for (int x = area.getX(); x < area.getX() + area.getWidth(); x++) {
             for (int y = area.getY(); y < area.getY() + area.getHeight(); y++) {
