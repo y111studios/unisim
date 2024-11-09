6cd645517a5996907bfba1b338c4836e167bda8f
Roboss11 <sam.knight0411@gmail.com>
2024-11-09 16:45:14 +0000

    Added documentation to VariantProperties interface

--- src/main/java/y111studios/buildings/premade_variants/VariantProperties.java
+++ src/main/java/y111studios/buildings/premade_variants/VariantProperties.java
@@ -1,9 +1,29 @@
 package y111studios.buildings.premade_variants;
 
-public interface VariantProperties{
+/**
+ * An interface representing the properties that all variants of a
+ * {@link y111studios.buildings.Building Building} must have. This interface is used as a type bound
+ * for generic variants to construct a building.
+ */
+public interface VariantProperties {
+    /**
+     * Returns the width of the variant.
+     * 
+     * @return the width of the variant
+     */
     int getWidth();
 
+    /**
+     * Returns the height of the variant.
+     * 
+     * @return the height of the variant
+     */
     int getHeight();
 
+    /**
+     * Returns the class object of the variant.
+     * 
+     * @return the class of the variant
+     */
     Class<? extends Enum<?>> getVariantClass();
 }
